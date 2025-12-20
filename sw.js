057596287b6eea63138b651cb23af0786e4b2d1a
bazzal831-png <bazzal831@gmail.com>
2025-12-20 09:40:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // اسم الكاش - قم بتغيير الرقم (v4) عند تحديث الكود لفرض التحديث على الهواتف
-const CACHE_NAME = "solo-leveling-system-v4";
+const CACHE_NAME = "solo-leveling-system-v5";
 
 // قائمة الملفات المطلوب حفظها للعمل بدون إنترنت (Offline)
 // يجب كتابة المسار الكامل للمجلد في GitHub Pages
@@ -50,6 +50,7 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+
 
 
 
